13c5f071d1ce14fbf2a230d93957b5513fe10a9d
Bruno Gomes <brunongomes8@gmail.com>
2025-05-21 13:58:25 -0300

    feat: implement method to search for company by ID and cards

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empresa;
+use Illuminate\Http\Request;
+
+class EmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $empresa = Empresa::with('cartoes')->find($id);
+
+        if (!$empresa) {
+            return response()->json(['message' => 'Empresa not found'], 404);
+        }
+
+        return response()->json($empresa);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
